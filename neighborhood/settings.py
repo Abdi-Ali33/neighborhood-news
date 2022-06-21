ae37b6838e1b7644c871c6a6391c2d215f39fbcc
Abdi Ali <alikune33@gmail.com>
2022-06-21 22:07:16 +0300

    deploying to heroku

--- neighborhood/settings.py
+++ neighborhood/settings.py
@@ -148,13 +148,10 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/var/www/static/',
-]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
